93158a8617c5df7bc54412265c87263244e65ba7
Mouaad Aallam <mouaad@aallam.com>
2021-03-08 17:45:18 +0100

    chore: add native sample

--- sample/native/src/nativeMain/kotlin/main.kt
+++ sample/native/src/nativeMain/kotlin/main.kt
@@ -1,16 +1,21 @@
-package com.aallam.openai.sample.jvm
+//fun main() {
+//  println("Hello, Kotlin/Native!")
+//}
 
 import com.aallam.openai.api.completion.CompletionRequest
 import com.aallam.openai.api.engine.Engine
 import com.aallam.openai.api.engine.EngineId
 import com.aallam.openai.api.search.SearchRequest
 import com.aallam.openai.client.OpenAI
+import kotlinx.cinterop.toKString
 import kotlinx.coroutines.runBlocking
+import platform.posix.getenv
 
-fun main() = runBlocking {
-    val apiKey = System.getenv("OPENAI_API_KEY")
+fun main(): Unit = runBlocking {
+
+    val apiKey = getenv("OPENAI_API_KEY")?.toKString()
     val token = requireNotNull(apiKey) { "OPENAI_API_KEY environment variable must be set." }
-    val openAI = OpenAI(token)
+    val openAI = OpenAI(token = token)
 
     println("> Getting available engines...")
     openAI.engines().forEach(::println)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,7 @@ rootProject.name = "OpenAI-Kotlin"
 
 include(":openai-core")
 include(":openai-client")
+
 include(":sample:jvm")
 include(":sample:js")
+include(":sample:native")

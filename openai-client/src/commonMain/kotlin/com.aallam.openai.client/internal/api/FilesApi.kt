a48d0330a77cb9fe37a313f9aa65edcebd741865
Mouaad Aallam <mouaad@aallam.com>
2022-03-20 12:55:31 +0100

    fix: file request content type

--- openai-client/src/commonMain/kotlin/com.aallam.openai.client/internal/api/FilesApi.kt
+++ openai-client/src/commonMain/kotlin/com.aallam.openai.client/internal/api/FilesApi.kt
@@ -8,16 +8,16 @@ import com.aallam.openai.client.Files
 import com.aallam.openai.client.internal.http.HttpTransport
 import io.ktor.client.call.body
 import io.ktor.client.request.delete
-import io.ktor.client.request.forms.FormBuilder
-import io.ktor.client.request.forms.append
-import io.ktor.client.request.forms.formData
-import io.ktor.client.request.forms.submitFormWithBinaryData
+import io.ktor.client.request.forms.*
 import io.ktor.client.request.get
 import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.PartData
+import io.ktor.http.contentType
+import io.ktor.util.InternalAPI
+import io.ktor.util.reflect.typeInfo
 import okio.FileSystem
 import okio.Path.Companion.toPath
 
@@ -29,13 +29,16 @@ internal class FilesApi(
     private val fileSystem: FileSystem
 ) : Files {
 
+    @OptIn(InternalAPI::class)
     override suspend fun file(request: FileRequest): File {
         val data: List<PartData> = formData {
             appendFile(fileSystem, "file", request.file)
             append("purpose", request.purpose.raw)
         }
         return httpRequester.perform {
-            it.submitFormWithBinaryData(url = FilesPath, formData = data).body()
+            it.submitFormWithBinaryData(url = FilesPath, formData = data) {
+                contentType(ContentType.MultiPart.FormData)
+            }
         }
     }
 
@@ -44,7 +47,9 @@ internal class FilesApi(
     }
 
     override suspend fun file(fileId: FileId): File? {
-        val response = httpRequester.perform<HttpResponse> { it.get { url(path = "$FilesPath/$fileId") } }
+        val response = httpRequester.perform<HttpResponse> {
+            it.get { url(path = "$FilesPath/$fileId") }
+        }
         return if (response.status == HttpStatusCode.NotFound) null else response.body()
     }
 

da9e0012b77baecfcbe8e1250178c664eb4e8aca
Mouaad Aallam <mouaad@aallam.com>
2022-07-09 12:26:57 +0200

    chore: generate documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import com.diffplug.gradle.spotless.SpotlessExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
@@ -9,7 +10,7 @@ plugins {
     alias(libs.plugins.kotlinx.binary.validator) apply false
     alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.spotless) apply false
-    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.dokka)
 }
 
 subprojects {
@@ -29,4 +30,8 @@ subprojects {
             showStandardStreams = false
         }
     }
+}
+
+tasks.named<DokkaMultiModuleTask>("dokkaHtmlMultiModule") {
+    outputDirectory.set(projectDir.resolve("docs"))
 }

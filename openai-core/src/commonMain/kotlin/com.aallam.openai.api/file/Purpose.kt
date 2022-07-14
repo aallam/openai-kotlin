e6bb190fbe6ecf3c9da4edb8e6d80e273d0533e1
Mouaad Aallam <mouaad@aallam.com>
2022-07-14 18:10:41 +0200

    remove file purpose for deprecated endpoints

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/file/Purpose.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/file/Purpose.kt
@@ -1,24 +1,17 @@
 package com.aallam.openai.api.file
 
-import com.aallam.openai.api.file.internal.PurposeSerializer
 import kotlinx.serialization.Serializable
 import kotlin.jvm.JvmInline
 
-@Serializable(PurposeSerializer::class)
+@Serializable
 @JvmInline
-public value class Purpose(public val raw: String)
+public value class Purpose(public val raw: String) {
 
-/**
- * File for Searches.
- */
-public val Search: Purpose = Purpose("search")
+    public companion object {
 
-/**
- * File for Answers.
- */
-public val Answers: Purpose = Purpose("answers")
-
-/**
- * File for classifications.
- */
-public val Classifications: Purpose = Purpose("classifications")
+        /**
+         * File for Searches.
+         */
+        public val FineTune: Purpose = Purpose("fine-tune")
+    }
+}

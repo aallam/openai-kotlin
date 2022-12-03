f3b80cda310e6278e8ae5ec58f06a4ed81636f61
Mouaad Aallam <mouaad@aallam.com>
2022-12-03 13:14:20 +0100

    fix jvm conflict

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/file/FileRequest.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/file/FileRequest.kt
@@ -1,6 +1,8 @@
 package com.aallam.openai.api.file
 
 import com.aallam.openai.api.ExperimentalOpenAI
+import kotlin.jvm.JvmName
+import kotlin.jvm.JvmSynthetic
 
 /**
  * File API request.
@@ -11,11 +13,11 @@ public data class FileRequest @ExperimentalOpenAI constructor(
     /**
      * JSON Lines file to be uploaded.
      *
-     * If the [purpose] is set to [Search] or [Answers], each line is a JSON record with a "text" field and an optional
+     * If the [purpose] is set to Search or Answers, each line is a JSON record with a "text" field and an optional
      * "metadata" field. Only "text" field will be used for search. Specially, when the purpose is "answers", "\n"
      * is used as a delimiter to chunk contents in the "text" field into multiple documents for finer-grained matching.
      *
-     * If the [purpose] is set to [Classifications], each line is a JSON record with a single training example with "text"
+     * If the [purpose] is set to Classifications, each line is a JSON record with a single training example with "text"
      * and "label" fields along with an optional "metadata" field.
      */
     public val filePath: FilePath,
@@ -29,9 +31,13 @@ public data class FileRequest @ExperimentalOpenAI constructor(
     public val purpose: Purpose,
 ) {
 
-
-    public constructor(file: String, purpose: Purpose) : this(FilePath(file), purpose)
-
     public val file: String
         get() = filePath.path
+
+    public companion object {
+
+        @JvmSynthetic
+        @JvmName("create")
+        public operator fun invoke(file: String, purpose: Purpose): FileRequest = FileRequest(FilePath(file), purpose)
+    }
 }

b93d644dbe3a461fd9d25194d0bc14241ed3b762
Mouaad Aallam <mouaad@aallam.com>
2022-12-11 19:40:14 +0100

    feat: add configurable timeouts

---
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/http/Timeout.kt
@@ -0,0 +1,18 @@
+package com.aallam.openai.api.http
+
+import com.aallam.openai.api.ExperimentalOpenAI
+import kotlin.time.Duration
+
+/**
+ * Http operations timeouts.
+ *
+ * @param request time period required to process an HTTP call: from sending a request to receiving a response
+ * @param connect time period in which a client should establish a connection with a server
+ * @param socket maximum time of inactivity between two data packets when exchanging data with a server
+ */
+@ExperimentalOpenAI
+public class Timeout(
+    public val request: Duration? = null,
+    public val connect: Duration? = null,
+    public val socket: Duration? = null,
+)

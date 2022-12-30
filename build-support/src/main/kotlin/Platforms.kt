8276de08025d3dd400310ddf83c828cab0fb0c12
Mouaad Aallam <mouaad@aallam.com>
2022-12-30 23:38:09 +0100

    chore: add darwin x64/arm64 targets

--- build-support/src/main/kotlin/Platforms.kt
+++ build-support/src/main/kotlin/Platforms.kt
@@ -1,21 +1,36 @@
 import org.gradle.kotlin.dsl.creating
 import org.gradle.kotlin.dsl.getValue
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 fun KotlinMultiplatformExtension.native() {
-    val targets = mutableListOf<KotlinNativeTarget>().apply {
-        add(linuxX64())
-        add(macosX64())
-        add(macosArm64())
-        add(mingwX64())
-    }
     sourceSets.apply {
+        // Native targets
         val nativeMain by creating { dependsOn(getByName("commonMain")) }
         val nativeTest by creating { dependsOn(getByName("commonTest")) }
-        targets.forEach { target ->
-            getByName("${target.name}Main").dependsOn(nativeMain)
-            getByName("${target.name}Test").dependsOn(nativeTest)
+
+        // Desktop targets
+        val desktopMain by creating { dependsOn(nativeMain) }
+        val desktopTest by creating { dependsOn(nativeTest) }
+        listOf(linuxX64(), mingwX64()).forEach { target ->
+            getByName("${target.name}Main").dependsOn(desktopMain)
+            getByName("${target.name}Test").dependsOn(desktopTest)
+        }
+
+        // Darwin targets
+        val darwinMain by creating { dependsOn(nativeMain) }
+        val darwinTest by creating { dependsOn(nativeTest) }
+        listOf(
+            iosX64(),
+            iosArm64(),
+            macosX64(),
+            macosArm64(),
+            tvosX64(),
+            tvosArm64(),
+            watchosArm64(),
+            watchosX64(),
+        ).forEach { target ->
+            getByName("${target.name}Main").dependsOn(darwinMain)
+            getByName("${target.name}Test").dependsOn(darwinTest)
         }
     }
 }

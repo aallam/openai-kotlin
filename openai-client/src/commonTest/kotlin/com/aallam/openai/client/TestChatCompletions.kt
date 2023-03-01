d9eb3989378026966abe2dcf83960457312c714d
Mouaad Aallam <mouaad@aallam.com>
2023-03-02 00:53:06 +0100

    add request dsl

--- openai-client/src/commonTest/kotlin/com/aallam/openai/client/TestChatCompletions.kt
+++ openai-client/src/commonTest/kotlin/com/aallam/openai/client/TestChatCompletions.kt
@@ -1,9 +1,9 @@
 package com.aallam.openai.client
 
 import com.aallam.openai.api.chat.ChatCompletion
-import com.aallam.openai.api.chat.ChatCompletionRequest
-import com.aallam.openai.api.chat.ChatMessage
 import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.api.chat.chatCompletionRequest
+import com.aallam.openai.api.chat.chatMessage
 import com.aallam.openai.api.model.ModelId
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -17,15 +17,15 @@ class TestChatCompletions : TestOpenAI() {
     @Test
     fun chatCompletions() {
         runTest {
-            val request = ChatCompletionRequest(
+            val request = chatCompletionRequest {
                 model = ModelId("gpt-3.5-turbo"),
                 messages = listOf(
-                    ChatMessage(
+                    chatMessage {
                         role = ChatRole.User,
                         content = "Hello!"
-                    )
+                    }
                 )
-            )
+            }
 
             val completion = openAI.chatCompletion(request)
             assertTrue { completion.choices.isNotEmpty() }
--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/chat/ChatMessage.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/chat/ChatMessage.kt
@@ -24,3 +24,38 @@ public class ChatMessage(
     @SerialName("name") public val name: String? = null
 )
 
+/**
+ * The messages to generate chat completions for.
+ */
+public fun chatMessage(block: ChatMessageBuilder.() -> Unit): ChatMessage =
+    ChatMessageBuilder().apply(block).build()
+
+/**
+ * Builder of [ChatMessageBuilder] instances.
+ */
+public class ChatMessageBuilder {
+
+    /**
+     * The role of the author of this message.
+     */
+    public var role: ChatRole? = null
+
+    /**
+     * The contents of the message.
+     */
+    public var content: String? = null
+
+    /**
+     * The name of the user in a multi-user chat.
+     */
+    public var name: String? = null
+
+    /**
+     * Create [ChatMessageBuilder] instance.
+     */
+    public fun build(): ChatMessage = ChatMessage(
+        role = requireNotNull(role) { "role is required" },
+        content = requireNotNull(content) { "content is required" },
+        name = name,
+    )
+}

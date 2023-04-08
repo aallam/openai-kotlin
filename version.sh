325359b8e27c053ee7311310f156585305fa8bc0
Mouaad Aallam <mouaad@aallam.com>
2023-04-08 19:06:06 +0200

    chore: remove unneeded files [skip ci]

--- version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-VERSION_NAME=$(grep "^VERSION_NAME=" gradle.properties | cut -d '=' -f 2)
-          if [[ $VERSION_NAME == *-SNAPSHOT ]]; then
-            echo "::set-output name=is_snapshot::true"
-          else
-            echo "::set-output name=is_snapshot::false"
-          fi

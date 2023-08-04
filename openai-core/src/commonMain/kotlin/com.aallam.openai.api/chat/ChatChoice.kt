cb0ca65e663e7597cb6f3adc4f4e9988b88c1509
Mouaad Aallam <mouaad@aallam.com>
2023-08-04 15:27:57 +0200

    chat choice required fields

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/chat/ChatChoice.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/chat/ChatChoice.kt
@@ -15,14 +15,14 @@ public data class ChatChoice(
     /**
      * Chat choice index.
      */
-    @SerialName("index") public val index: Int? = null,
+    @SerialName("index") public val index: Int,
     /**
      * The generated chat message.
      */
-    @SerialName("message") public val message: ChatMessage? = null,
+    @SerialName("message") public val message: ChatMessage,
 
     /**
      * The reason why OpenAI stopped generating.
      */
-    @SerialName("finish_reason") public val finishReason: String? = null,
+    @SerialName("finish_reason") public val finishReason: String,
 )

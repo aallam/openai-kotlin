4de486f2abda1c4100089b88c637cbc10828da36
Mouaad Aallam <mouaad@aallam.com>
2023-08-04 16:19:24 +0200

    mark edits as deprecated

--- openai-client/src/commonMain/kotlin/com.aallam.openai.client/Edits.kt
+++ openai-client/src/commonMain/kotlin/com.aallam.openai.client/Edits.kt
@@ -6,10 +6,12 @@ import com.aallam.openai.api.edits.EditsRequest
 /**
  * Given a prompt and an instruction, the model will return an edited version of the prompt.
  */
+@Deprecated("Edits is deprecated. Chat completions is the recommend replacement.")
 public interface Edits {
 
     /**
      * Creates a new edit for the provided input, instruction, and parameters.
      */
+    @Deprecated("Use chat completions instead.")
     public suspend fun edit(request: EditsRequest): Edit
 }
--- openai-client/src/commonMain/kotlin/com.aallam.openai.client/internal/api/EditsApi.kt
+++ openai-client/src/commonMain/kotlin/com.aallam.openai.client/internal/api/EditsApi.kt
@@ -5,12 +5,9 @@ import com.aallam.openai.api.edits.EditsRequest
 import com.aallam.openai.client.Edits
 import com.aallam.openai.client.internal.http.HttpRequester
 import com.aallam.openai.client.internal.http.perform
-import io.ktor.client.call.body
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.client.request.url
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 
 /**
  * Implementation of [Edits]

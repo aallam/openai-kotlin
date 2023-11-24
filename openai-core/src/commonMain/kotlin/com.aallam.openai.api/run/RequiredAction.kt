5554118d91c1845b33e46bf1733cf05682f540c0
Voqal Developer <150201955+voqaldev@users.noreply.github.com>
2023-11-24 17:10:01 -0600

    fix(runs): field 'tool_calls' is required for type with serial name 'submit_tool_outputs', but it was missing (#266)

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/run/RequiredAction.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/run/RequiredAction.kt
@@ -1,10 +1,12 @@
 package com.aallam.openai.api.run
 
 import com.aallam.openai.api.BetaOpenAI
-import com.aallam.openai.api.chat.ToolCall
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Details on the action required to continue the run.
+ */
 @BetaOpenAI
 @Serializable
 public sealed interface RequiredAction {
@@ -15,6 +17,6 @@ public sealed interface RequiredAction {
         /**
          * A list of the relevant tool calls.
          */
-        @SerialName("tool_calls") public val toolCalls: List<ToolCall>,
+        @SerialName("submit_tool_outputs") public val toolOutputs: ToolOutputs,
     ) : RequiredAction
 }

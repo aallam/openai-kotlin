64be561d8df113e5d98c15ec1d5d607a2c7920e3
Mouaad Aallam <mouaad@aallam.com>
2024-02-10 20:04:16 +0100

    fix(finetuning): nullable message and code

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/finetuning/ErrorInfo.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/finetuning/ErrorInfo.kt
@@ -11,12 +11,12 @@ public data class ErrorInfo(
     /**
      * A human-readable error message.
      */
-    val message: String,
+    val message: String? = null,
 
     /**
      * A machine-readable error code.
      */
-    val code: String,
+    val code: String? = null,
 
     /**
      * The parameter that was invalid (e.g., `training_file`, `validation_file`), or null if not parameter-specific.

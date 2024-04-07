8e847ded935c2093a9c4ae8d013e49bb95ac7222
Mouaad Aallam <mouaad@aallam.com>
2024-04-07 13:54:33 +0200

    fix: remove quality typealias

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/Quality.kt
+++
@@ -1,5 +0,0 @@
-/**
- * The quality of the image that will be generated
- */
-@Deprecated("This class is deprecated.", ReplaceWith("Quality", "com.aallam.openai.api.image.Quality"))
-public typealias Quality = com.aallam.openai.api.image.Quality

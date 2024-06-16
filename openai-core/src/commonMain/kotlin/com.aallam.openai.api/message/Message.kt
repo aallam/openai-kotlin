240a8a8db19727e33344acb1ee3564862eed78f0
SF93 <kdman98@naver.com>
2024-06-16 14:24:28 +0900

    forgotten removal of file_ids from message

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/message/Message.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/message/Message.kt
@@ -53,12 +53,6 @@ public data class Message(
     @SerialName("run_id") val runId: RunId? = null,
 
     /**
-     * A list of file IDs that the assistant should use. Useful for tools like retrieval
-     * and code_interpreter that can access files. A maximum of 10 files can be attached to a message.
-     */
-    @SerialName("file_ids") val fileIds: List<FileId>,
-
-    /**
      * Set of 16 key-value pairs that can be attached to an object. This can be useful for storing
      * additional information about the object in a structured format. Keys can be a maximum of 64 characters
      * long, and values can be a maximum of 512 characters long.

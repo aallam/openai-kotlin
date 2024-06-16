c1c6972cbb13ec65b6dcbb1bf1c7e010bd007a0b
SF93 <kdman98@naver.com>
2024-06-16 14:24:28 +0900

    forgotten removal of file_ids from message

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/message/MessageRequest.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/message/MessageRequest.kt
@@ -23,13 +23,6 @@ public class MessageRequest(
     @SerialName("content") public val content: String,
 
     /**
-     * A list of File IDs that the message should use.
-     * There can be a maximum of 10 files attached to a message.
-     * Useful for tools like retrieval and code interpreter that can access and use files.
-     */
-    @SerialName("file_ids") public val fileIds: List<FileId>? = null,
-
-    /**
      * Set of 16 key-value pairs that can be attached to an object.
      * This can be useful for storing additional information about the object in a structured format.
      * Keys can be a maximum of 64 characters long, and values can be a maximum of 512 characters long.
@@ -60,13 +53,6 @@ public class MessageRequestBuilder {
     public var content: String? = null
 
     /**
-     * A list of File IDs that the message should use.
-     * There can be a maximum of 10 files attached to a message.
-     * Useful for tools like retrieval and code interpreter that can access and use files.
-     */
-    public var fileIds: List<FileId>? = null
-
-    /**
      * Set of 16 key-value pairs that can be attached to an object.
      * This can be useful for storing additional information about the object in a structured format.
      * Keys can be a maximum of 64 characters long, and values can be a maximum of 512 characters long.
@@ -76,7 +62,6 @@ public class MessageRequestBuilder {
     public fun build(): MessageRequest = MessageRequest(
         role = requireNotNull(role) { "role is required" },
         content = requireNotNull(content) { "content is required" },
-        fileIds = fileIds,
         metadata = metadata
     )
 }

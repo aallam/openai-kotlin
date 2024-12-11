66ab4a45a153198fd7f059976974c3f859f9238e
Kristian Freed <kristian@slingshot.xyz>
2024-12-11 10:20:19 +0000

    Add support for detailed usage data

--- openai-core/src/commonMain/kotlin/com.aallam.openai.api/core/Usage.kt
+++ openai-core/src/commonMain/kotlin/com.aallam.openai.api/core/Usage.kt
@@ -17,4 +17,24 @@ public data class Usage(
      * Count of total tokens.
      */
     @SerialName("total_tokens") public val totalTokens: Int? = null,
+
+    @SerialName("completion_tokens_details")
+    val completionTokenDetails: CompletionTokenUsageDetails? = null,
+
+    @SerialName("prompt_tokens_details")
+    val promptTokenDetails: PromptTokenUsageDetails? = null
+)
+
+@Serializable
+public data class PromptTokenUsageDetails(
+    @SerialName("audio_tokens") public val audioTokens: Int? = null,
+    @SerialName("cached_tokens") public val cachedTokens: Int? = null,
+)
+
+@Serializable
+public data class CompletionTokenUsageDetails(
+    @SerialName("audio_tokens") public val audioTokens: Int? = null,
+    @SerialName("reasoning_tokens") public val reasoningTokens: Int? = null,
+    @SerialName("accepted_prediction_tokens") public val acceptedPredictionTokens: Int? = null,
+    @SerialName("rejected_prediction_tokens") public val rejectedPredictionTokens: Int? = null,
 )
